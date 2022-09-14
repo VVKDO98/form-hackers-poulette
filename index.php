27f17d1d8b1023ede346a201bc5588bb5d1e399c
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-09-14 09:22:45 +0200

    Init commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact Hackers Poulette</title>
+</head>
+<body>
+    <main>
+        <form action="" method="post">
+            <div class="form__name">
+                <label for="name">Enter your name</label>
+                <input type="text" name="name">
+            </div>
+            <div class="form__firstname">
+                <label for="firstname">Enter your firstname</label>
+                <input type="text" name="firstname">
+            </div>
+            <div class="form__mail">
+                <label for="mail">Enter your mail</label>
+                <input type="text" name="mail">
+            </div>
+            <div class="form__description">
+                <label for="description">Describe the problem</label>
+                <input type="text" name="description">
+            </div>
+        </form>
+    </main>
+</body>
+</html>
